4187759df60906ba753270a0a7ddbc4d55573985
kmilo111 <ingcamilomunoz@hotmail.com>
2021-07-26 09:57:19 -0500

    delete console.log

--- src/controller/service/impl/risk.service.impl.ts
+++ src/controller/service/impl/risk.service.impl.ts
@@ -78,7 +78,6 @@ export class RiskService implements IRiskService {
   async validatePaymentService(data){
 
     const resPay = await this.paymentProvider.consumePaymentReferencesMgmt(data);
-    // console.log(resPay);
     return resPay;
   }
 
