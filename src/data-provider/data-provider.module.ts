50bcc82c013c6ed8f1a8907b0be0f9f64bb4046d
Jhon Buesaquillo <jhon.edwar.b.salazar@accenture.com>
2021-08-11 10:47:30 -0500

    Agregando creación por defecto de mensajes en bd

--- src/data-provider/data-provider.module.ts
+++ src/data-provider/data-provider.module.ts
@@ -1,11 +1,15 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MESSAGE } from 'src/common/configuration/messages/message-config';
 import databaseConfig from '../common/configuration/database.config';
 import servicesConfig from '../common/configuration/services.config';
 import { IMessageProvider } from './message.provider';
 import { MessageModel, MessageSchema } from './model/message.model';
-import { TraceabilityModel, TraceabilitySchema } from './model/traceability.model';
+import {
+  TraceabilityModel,
+  TraceabilitySchema,
+} from './model/traceability.model';
 import { MessageProvider } from './provider/message.provider.impl';
 import { TraceabilityProvider } from './provider/traceability.provider.impl';
 import { ITraceabilityProvider } from './traceability.provider';
@@ -17,7 +21,7 @@ import { ITraceabilityProvider } from './traceability.provider';
       retryAttempts: 3,
       useCreateIndex: true,
       useFindAndModify: false,
-      autoCreate: true
+      autoCreate: true,
     }),
     MongooseModule.forFeature([
       {
@@ -28,15 +32,28 @@ import { ITraceabilityProvider } from './traceability.provider';
       {
         name: TraceabilityModel.name,
         schema: TraceabilitySchema,
-        collection: 'coll_traceability'
-      }
+        collection: 'coll_traceability',
+      },
     ]),
     HttpModule.registerAsync({
       useFactory: () => servicesConfig.httpConfig,
     }),
   ],
-  providers: [{ provide: IMessageProvider, useClass: MessageProvider },
-              { provide: ITraceabilityProvider, useClass: TraceabilityProvider }],
-  exports: [IMessageProvider, ITraceabilityProvider],
+  providers: [
+    { provide: IMessageProvider, useClass: MessageProvider },
+    { provide: ITraceabilityProvider, useClass: TraceabilityProvider },
+    {
+      provide: 'VerifyMessages',
+      useFactory: async (messageProvider: IMessageProvider) => {
+        const num = await messageProvider.getTotal({});
+        if (num == 0) {
+          // Si no hay mensajes en bd, se crean los mensajes por defecto
+          await messageProvider.createMessages(MESSAGE);
+        }
+      },
+      inject: [IMessageProvider]
+    },
+  ],
+  exports: [IMessageProvider, ITraceabilityProvider, 'VerifyMessages'],
 })
 export class DataProviderModule {}
