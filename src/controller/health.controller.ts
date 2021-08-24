3ec090b5f88ecc3d7df4bd4a6598fc28b45e49fa
Jhon Buesaquillo <jhon.edwar.b.salazar@accenture.com>
2021-08-24 09:53:48 -0500

    Juntando health check de bd y servicio http en una sola petición

--- src/controller/health.controller.ts
+++ src/controller/health.controller.ts
@@ -1,42 +1,56 @@
 import { Controller, Get } from '@nestjs/common';
-import { Transport } from '@nestjs/microservices';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import {
   HealthCheck,
   HealthCheckService,
   HttpHealthIndicator,
-  MicroserviceHealthIndicator,
   MongooseHealthIndicator,
 } from '@nestjs/terminus';
 
+@ApiTags('healthCheck')
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
     private dbMongo: MongooseHealthIndicator,
     private http: HttpHealthIndicator,
-    private ms: MicroserviceHealthIndicator
   ) {}
 
-  @Get('/database')
-  @HealthCheck()
-  checkDatabase() {
-    return this.health.check([() => this.dbMongo.pingCheck('database_mongo')]);
+  @Get('')
+  @ApiOperation({ 
+    description: 'Health check de base de datos y servicio http' 
+  })
+  async checkDatabase() {
+    const result = {
+      success: true,
+      documents: {
+        database: await this.checkDB(),
+        serviceHttp: await this.checkServiceHttp(),
+      },
+    };
+    return result;
   }
 
-  @Get('/service')
   @HealthCheck()
-  checkService() {
-    return this.http.pingCheck('service-example', 'http://localhost:8080');
+  private async checkDB() {
+    try {
+      const n = await this.health.check([() => this.dbMongo.pingCheck('database_mongo')]);
+      return n;
+    } catch (error) {
+      return error.response;
+    }
   }
 
-//   @Get('/service')
-//   @HealthCheck()
-//   checkService() {
-//     return this.health.check([
-//       () => this.ms.pingCheck('service-example', {
-//         transport: Transport.NATS,
-//         options: { host: 'localhost', port: 8080 },
-//       })
-//     ]);
-//   }
+  @HealthCheck()
+  private async checkServiceHttp() {
+    let n: any;
+    try {
+      n = await this.http.pingCheck('service-example', 'http://localhost:8080');
+    } catch (error) {
+      n = error.causes;
+    } finally { 
+      n['service-example']['source'] = 'http://localhost:8080';
+      return n;
+    }
+  }
 }
